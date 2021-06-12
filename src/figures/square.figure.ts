d818d921cff799eeb4bdfac72ce0a1d18546d452
Eugene Palshin <eo.palshin@gmail.com>
2021-06-12 08:33:36 +0300

    Remove extra method

--- src/figures/square.figure.ts
+++ src/figures/square.figure.ts
@@ -1,10 +1,8 @@
 import { Figure } from '@/figure';
 import { Position } from '@/types/position.interface';
 import { Block } from '@/block';
-import { Movable } from '@/types/movable.interface';
-import { MoveDirection } from '@/types/move-direction.type';
 
-export class Square extends Figure implements Movable {
+export class Square extends Figure {
   constructor(protected readonly position: Position, protected readonly color: string) {
     super(position, color);
 
@@ -14,14 +12,5 @@ export class Square extends Figure implements Movable {
       new Block({ x: position.x, y: position.y + 1 }, color),
       new Block({ x: position.x + 1, y: position.y + 1 }, color),
     ];
-  }
-
-  move(direction: MoveDirection, step = 1): Square {
-    const square = new Square(this.position, this.color);
-    for (const block of square.blocks) {
-      block.move(direction, step);
-    }
-
-    return square;
   }
 }
